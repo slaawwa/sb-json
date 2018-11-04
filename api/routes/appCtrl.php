940527114fa090aceb23dd25eafed076014a7e1d
Слава Кушнір <slaawwa@mail.ru>
2018-11-04 20:27:52 +0000

    Fix bug with return default file in php

--- api/routes/appCtrl.php
+++ api/routes/appCtrl.php
@@ -6,12 +6,12 @@ return [
     'path' => '/api/?cmd=admin-',
     'withGET' => true,
     'method' => '*',
-    'handler' => function() use ($errorFile) {
+    'handler' => function($body, $cnf) use ($errorFile) {
         $file = App::scanDir(substr( $_GET['cmd'], 6 ));
         $ext = pathinfo($file)['extension'];
         if ($ext === 'php') {
             $f = require($file);
-            $file = $f['default'];
+            $file = $cnf->folder['dist'] . '/' . $f['default'];
         }
         $file_exist = file_exists($file);
         $data = file_get_contents($file_exist? $file: $errorFile);
